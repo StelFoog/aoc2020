5ad050792c13dde27ccefb3e8d3ac4eae18dde3f
samuel <samuel@familjenlarsson.eu>
2020-12-03 08:52:20 +0100

    Day 3

---
+++ 3-1.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+
+fs.readFile("./input", "UTF-8", (error, data) => {
+	if (error) {
+		console.error(error);
+		return;
+	}
+	// Code
+	const vertical = data.split("\n").filter(s => s != " ");
+	const horizontalLength = vertical[0].length;
+	var pos = { x: 0, y: 0 };
+	var treesHit = 0;
+	while(pos.x < vertical.length) {
+		if(vertical[pos.x].charAt(pos.y % horizontalLength) === "#")
+			treesHit++;
+		pos.x += 1;
+		pos.y += 3;
+	}
+	console.log(treesHit);
+})
---
+++ 3-2.js
@@ -0,0 +1,37 @@
+const fs = require("fs");
+
+fs.readFile("./input", "UTF-8", (error, data) => {
+	if (error) {
+		console.error(error);
+		return;
+	}
+	// Code
+	const vertical = data.split("\n").filter(s => s != " ");
+	const horizontalLength = vertical[0].length;
+	const directions = [
+		{x: 1, y: 1},
+		{x: 1, y: 3},
+		{x: 1, y: 5},
+		{x: 1, y: 7},
+		{x: 2, y: 1},
+	];
+	var treesHit = [];
+	directions.forEach(() => {
+		treesHit.push(0);
+	});
+	directions.forEach((dir, index) => {
+		var pos = { x: 0, y: 0 };
+		while(pos.x < vertical.length) {
+			if(vertical[pos.x].charAt(pos.y % horizontalLength) === "#")
+				treesHit[index]++;
+			pos.x += dir.x;
+			pos.y += dir.y;
+		}
+	});
+
+	var res = 1;
+	treesHit.forEach(e => {
+		res *= e;
+	});
+	console.log(res);
+})
